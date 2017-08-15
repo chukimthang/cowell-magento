835421f31e7a3dd751e4a2bf7f68d826a03fc07a
Chu Kim Thang <chukimthang94@gmail.com>
2017-08-15 11:28:10 +0700

    Model

--- app/code/Robin/Bai2/Setup/UpgradeSchema.php
+++ app/code/Robin/Bai2/Setup/UpgradeSchema.php
@@ -5,15 +5,14 @@ use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\DB\Ddl\Table;
 
-class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
+class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
 {
-    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-
         $connection = $setup->getConnection();
         $tableName = $setup->getTable('banner');
-        // Check if the table already exists
+        // Check if the table is not exists
         if ($connection->isTableExists($tableName) != true) {
             // Create table
             $table = $connection->newTable($tableName)->addColumn(
@@ -31,10 +30,22 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
                 Table::TYPE_TEXT,
                 255,
                 ['nullable' => false, 'default' => '']
+            )->addColumn(
+                'sort_order',
+                Table::TYPE_SMALLINT,
+                255,
+                ['nullable' => false, 'default' => 0]
+            )->addColumn(
+                'status',
+                Table::TYPE_BOOLEAN,
+                null,
+                ['nullable' => false, 'default' => false]
             )->setOption('charset', 'utf8');
             $connection->createTable($table);
+        } else {
+            // Add column "sort_order" and "status"
+            $setup->run("ALTER TABLE " . $tableName . " ADD COLUMN sort_order SMALLINT, ADD COLUMN status BOOLEAN;");
         }
-
         $setup->endSetup();
     }
 }
